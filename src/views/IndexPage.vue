08c84a2f21bf9013db0291d3208a84980483e712
YJXS-J <353667349@qq.com>
2022-08-10 13:33:09 +0800

    UTC_to_BJtime

--- src/views/IndexPage.vue
+++ src/views/IndexPage.vue
@@ -264,6 +264,9 @@ export default {
                 this.commitsArr.push(res[i].commit.committer);
                 this.commitsArr[i].message = res[i].commit.message;
             }
+            this.commitsArr.forEach(item => {
+                item.date = this.UTC_to_BJtime(item.date);
+            });
         } else {
             axios({
                 url: 'https://api.github.com/repos/YJXS-J/YJXS_Home_Page/commits',
@@ -279,6 +282,9 @@ export default {
                         this.commitsArr.push(res[i].commit.committer);
                         this.commitsArr[i].message = res[i].commit.message;
                     }
+                    this.commitsArr.forEach(item => {
+                        item.date = this.UTC_to_BJtime(item.date);
+                    });
                 },
                 // eslint-disable-next-line no-unused-vars
                 err => {}
@@ -525,6 +531,33 @@ export default {
             var week = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
             var weekDay = week[date.getDay()];
             this.time = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second + ' ' + weekDay;
+        },
+        UTC_to_BJtime(utc_datetime) {
+            // 转为正常的时间格式 年-月-日 时:分:秒
+            var T_pos = utc_datetime.indexOf('T');
+            var Z_pos = utc_datetime.indexOf('Z');
+            var year_month_day = utc_datetime.substr(0, T_pos);
+            var hour_minute_second = utc_datetime.substr(T_pos + 1, Z_pos - T_pos - 1);
+            var new_datetime = year_month_day + ' ' + hour_minute_second; // 2017-03-31 08:02:06
+
+            // 处理成为时间戳
+            timestamp = new Date(Date.parse(new_datetime));
+            timestamp = timestamp.getTime();
+            timestamp = timestamp / 1000;
+
+            // 增加8个小时，北京时间比utc时间多八个时区
+            var timestamp = timestamp + 8 * 60 * 60;
+
+            // 时间戳转为时间
+            var beijing_datetime = new Date(parseInt(timestamp) * 1000);
+            var year = beijing_datetime.getFullYear();
+            var month = ('0' + (beijing_datetime.getMonth() + 1)).slice(-2);
+            var day = ('0' + beijing_datetime.getDate()).slice(-2);
+            var hour = ('0' + beijing_datetime.getHours()).slice(-2);
+            var minute = ('0' + beijing_datetime.getMinutes()).slice(-2);
+            var second = ('0' + beijing_datetime.getSeconds()).slice(-2);
+            var datetime = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+            return datetime; // yyyy-mm-dd hh:mm:ss
         },
         comeBakc() {
             if (this.itemActive == '项目') {
